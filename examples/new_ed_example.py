42d251a753d1550b58f4f5f7265da3c142bccfc0
sponcg <sponchiadogiovanna@gmail.com>
2021-04-22 22:18:24 -0300

    Synch Updates

--- examples/new_ed_example.py
+++ examples/new_ed_example.py
@@ -10,23 +10,70 @@ import pluggins
 
 from machine.automaton import Automaton
 
-a = Automaton()
+'''
+g1 = Automaton()
+g2 = Automaton()
 
-e1 = a.event_add('a', False, True)
-e2 = a.event_add('b', True, True)
-e3 = a.event_add('c', True, True)
-s1 = a.state_add('q1', marked=True, initial=True)
-s2 = a.state_add('q2', marked=True)
+ev1 = g1.event_add('a', True, True)
+ev2 = g1.event_add('b', True, True)
 
-t1 = a.transition_add(s1, s2, e1)
-t2 = a.transition_add(s2, s1, e2, probability=0.2)
-a.transition_add(s1, s1, e2)
-a.transition_add(s2, s2, e1)
+s1 = g1.state_add('0', marked=True, initial=True)
+s2 = g1.state_add('1', marked=False)
+s3 = g1.state_add('2', marked=False)
 
-t1.probability = 0.5
+t1 = g1.transition_add(s1, s2, ev1)
+t2 = g1.transition_add(s2, s3, ev2)
+t3 = g1.transition_add(s3, s1, ev2)
 
-a.probabilitic_synchronization()
+ev3 = g2.event_add('b', True, True)
+ev4 = g2.event_add('g', True, True)
 
-print(a)
-print(t1.probabilistic_str())
-print(t2.probabilistic_str())
+s4 = g2.state_add('0', marked=True, initial=True)
+s5 = g2.state_add('1', marked=False)
+
+t4 = g2.transition_add(s4, s5, ev3)
+t5 = g2.transition_add(s5, s4, ev4)
+
+print(g1.synchronization(g2))
+'''
+
+sensor = Automaton()
+chave = Automaton()
+espec = Automaton()
+
+s1_on = sensor.event_add('s1_on', False, True)
+s1_off = sensor.event_add('s1_off', False, True)
+
+chave_on = chave.event_add('chave_on', True, True)
+chave_off = chave.event_add('chave_off', True, True)
+
+sensor1 = sensor.state_add('0', marked=True, initial=True)
+sensor2 = sensor.state_add('1', marked = False)
+
+chave1 = chave.state_add('0', marked=True, initial=True)
+chave2 = chave.state_add('1', marked=True)
+
+ts1 = sensor.transition_add(sensor1, sensor2, s1_on, probability=0.2)
+ts2 = sensor.transition_add(sensor2, sensor1, s1_off)
+
+tc1 = chave.transition_add(chave1, chave2, chave_on)
+tc2 = chave.transition_add(chave2, chave1, chave_off)
+
+ev1 = espec.event_add('chave_off', True, True)
+ev2 = espec.event_add('s1_on', False, True)
+ev3 = espec.event_add('chave_on', True, True)
+ev4 = espec.event_add('s1_off', False, True)
+
+espec1 = espec.state_add('0', marked=True, initial=True)
+espec2 = espec.state_add('1', marked=False)
+
+t1 = espec.transition_add(espec1, espec1, ev1)
+t2 = espec.transition_add(espec1, espec2, ev2)
+t3 = espec.transition_add(espec2, espec2, ev3)
+t4 = espec.transition_add(espec2, espec2, ev1)
+t5 = espec.transition_add(espec2, espec1, ev4)
+
+print(sensor.synchronization(chave, espec))
+#'''
+
+
