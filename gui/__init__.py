bebed133faa63ebd8e4688b83c2d463de0624b16
Yuri Kaszubowski Lopes <yurikazuba@gmail.com>
2025-01-29 13:14:11 -0300

    require_version of GtkSource to rm warning

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,5 +1,6 @@
 import gi
-gi.require_version("Gtk", "3.0")
+gi.require_version('Gtk', '3.0')
+gi.require_version('GtkSource', '3.0')
 from gi.repository import GLib, Gio, Gtk
 
 from gui.application import Application
